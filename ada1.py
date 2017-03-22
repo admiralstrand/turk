8e28c58276fdccaf0b3b2cee3219f770f2bcc582
charles.ogilvie@gmail.com <angostura69>
2017-03-22 23:16:44 +0000

    cleaned up code

--- ada1.py
+++ ada1.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python
 # Example using a character LCD connected to a Raspberry Pi or BeagleBone Black.
 import time
-
 import Adafruit_CharLCD as LCD
 
 
 # Raspberry Pi pin configuration:
-lcd_rs        = 7  # Note this might need to be changed to 21 for older revision Pi's.
+lcd_rs        = 7  
 lcd_en        = 8
 lcd_d4        = 25
 lcd_d5        = 24
@@ -14,22 +13,11 @@ lcd_d6        = 23
 lcd_d7        = 18
 lcd_backlight = 4
 
-# BeagleBone Black configuration:
-# lcd_rs        = 'P8_8'
-# lcd_en        = 'P8_10'
-# lcd_d4        = 'P8_18'
-# lcd_d5        = 'P8_16'
-# lcd_d6        = 'P8_14'
-# lcd_d7        = 'P8_12'
-# lcd_backlight = 'P8_7'
 
-# Define LCD column and row size for 16x2 LCD.
+# Define LCD column and row size 
 lcd_columns = 20
 lcd_rows    = 4
 
-# Alternatively specify a 20x4 LCD.
-# lcd_columns = 20
-# lcd_rows    = 4
 
 # Initialize the LCD using the pins above.
 lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,

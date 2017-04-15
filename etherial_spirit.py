b4af4a6564bb87c7c88a172af2ac584f3dd3d970
Ben <ben@notionparallax.co.uk>
2017-04-15 23:07:59 +1000

    matchy matchy

---
+++ etherial_spirit.py
@@ -0,0 +1,4 @@
+import sys
+sys.path.insert(0, '/var/www/html/etherial_spirit')
+
+from flaskapp import app as application
--- server/etherial_spirit.py
+++ server/etherial_spirit.py
@@ -3,8 +3,6 @@
 from flask import Flask
 # from flask import render_template
 from flask_socketio import SocketIO
-import sys
-sys.path.insert(0, '/var/www/html/turk')
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'

df8112ab87b8b0cbf6ef80d26decfe763ff8f9e4
Ben <ben@notionparallax.co.uk>
2017-04-15 10:50:13 +1000

    spoof file

---
+++ ben_shim.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""Mechanical Turk.
+
+Spoof the ada library
+"""
+
+
+def write_to_screen(thing):
+    """Pretend to write to screen, actually just print"""
+    print thing

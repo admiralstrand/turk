b42c3c17216bb112ceb261d2b77f33b75ada88e0
Ben <ben@notionparallax.co.uk>
2017-04-16 17:17:50 +1000

    new server code

--- server/etherial_spirit.py
+++ server/etherial_spirit.py
@@ -1,24 +1,26 @@
-# -*- coding: utf-8 -*-
-"""Super minimal app for the turk."""
 from flask import Flask
-# from flask import render_template
-from flask_socketio import SocketIO
+from flask_sockets import Sockets
+
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+sockets = Sockets(app)
+
+
+@sockets.route('/echo')
+def echo_socket(ws):
+    while not ws.closed:
+        message = ws.receive()
+        ws.send(message)
+        print message
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello boys!'
+def hello():
+    return 'Hello fake humans!'
 
 
-@socketio.on('message')
-def handle_message(message):
-    print('received message: ' + message)
-    SocketIO.send("I heard" + message)
-
-
-if __name__ == '__main__':
-    socketio.run(app)
+if __name__ == "__main__":
+    from gevent import pywsgi
+    from geventwebsocket.handler import WebSocketHandler
+    server = pywsgi.WSGIServer(('', 5000), app, handler_class=WebSocketHandler)
+    server.serve_forever()

5d7c0e5909cf7d8e40afdc013a1251dfc1b0bc98
Ben <ben@notionparallax.co.uk>
2017-04-19 03:55:50 +1000

    pub sub code

--- pi/ouija.py
+++ pi/ouija.py
@@ -33,32 +33,42 @@ def on_open(ws):
         print "here we go"
         t = turk.tappy_typing()
         while True:
-            # time.sleep(1)
             value = next(t)
             if value != "exit please":
-                payload = json.dumps({"handle": "turk",
+                payload = json.dumps({"handle": "turkClient",
                                       "text": value})
             else:
-                break
+                ws.close()
+                print "thread terminating..."
+                return True
             print "dump", payload
             ws.send(payload)
         time.sleep(1)
-        # ws.close()
-        # print "thread terminating..."
+    thread.start_new_thread(run, ())
+
+
+def listen(ws):
+    def run(*args):
+        pass
     thread.start_new_thread(run, ())
 
 
 if __name__ == "__main__":
     websocket.enableTrace(True)
     # server_address = "wss://echo.websocket.org"
-    # server_address = "ws://localhost:5000/echo"
-    # server_address = ("ws://ec2-52-40-215-205.us-west-2.compute."
-    #                   "amazonaws.com/echo")
-    server_address = "ws://nameless-dusk-67549.herokuapp.com/submit"
-    # server_address = "ws://localhost:5000/submit"
-    ws = websocket.WebSocketApp(server_address,
-                                on_message=on_message,
-                                on_error=on_error,
-                                on_close=on_close)
-    ws.on_open = on_open
-    ws.run_forever()
+    # server_address = "ws://nameless-dusk-67549.herokuapp.com/submit"
+    server_address = "ws://localhost:5000"
+
+    in_ws = websocket.WebSocketApp(server_address + "/receive",
+                                   on_message=on_message,
+                                   on_error=on_error,
+                                   on_close=on_close)
+    in_ws.on_open = listen
+    thread.start_new_thread(in_ws.run_forever, ())
+
+    out_ws = websocket.WebSocketApp(server_address + "/submit",
+                                    on_message=on_message,
+                                    on_error=on_error,
+                                    on_close=on_close)
+    out_ws.on_open = on_open
+    out_ws.run_forever()

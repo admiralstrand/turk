e063a7f733926b41b32ce01a00c511f820dab484
Ben <ben@notionparallax.co.uk>
2017-05-07 19:46:41 +1000

    move preview into text helpers

--- pi/ben_shim.py
+++ pi/ben_shim.py
@@ -3,29 +3,12 @@
 
 Spoof the ada library
 """
-import math
 import text_input_helpers as tt
 
 
 def write_to_screen(thing, cursor_pos=None, wide=20, high=4):
     """Pretend to write to screen, actually just print."""
-    lines_of_text = thing.split("\n")
-    right_edge = list("|" * 4)
-    top_row = "-" * 20
-
-    message = ""
-    if cursor_pos:
-        cursor_pos = tt.clamp(cursor_pos, wide * high)
-        column = cursor_pos % wide
-        row = int(math.floor(cursor_pos/wide))
-        row = tt.clamp(row, 3)  # 80/4 is 4, but 0123 rows - overflow
-        top_row = top_row[:cursor_pos-1] + "|" + top_row[cursor_pos:]
-        set_cursor(column, row)
-        right_edge[row] = "<-"
-    message += "|" + top_row + "\n"
-    for i, line in enumerate(lines_of_text):
-        message += "|" + line + right_edge[i] + "\n"
-    print message
+    tt.preview(thing, cursor_pos, wide, high)
 
 
 def set_cursor(col, row):
